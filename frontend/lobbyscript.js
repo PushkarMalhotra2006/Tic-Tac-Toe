72fc399b29a072d0f2074cc5ac2583ce81786510
PushkarMalhotra2006 <pushkarpmmal20@gmail.com>
2026-06-20 17:53:54 +0530

    finished the frontend of online mode pages , added a choose username featuure

--- frontend/lobbyscript.js
+++ frontend/lobbyscript.js
@@ -7,12 +7,21 @@ backbutton.addEventListener("click", () => {
 const roomCodeDisplay = document.getElementById("room-code");
 const copyBtn = document.getElementById("copy-code-btn");
 const tooltip = document.getElementById("tooltip");
+const startBtn = document.getElementById("start-btn");
 
 const params = new URLSearchParams(window.location.search);
 const roomCode = params.get("room");
+const isHost = params.get("host") === "true";
+const username = params.get("name");
 
 if (roomCode) {
     roomCodeDisplay.textContent = roomCode;
+}
+
+const P1_name = document.getElementById("p1-name");
+
+if(isHost){
+    P1_name.textContent = `${username} (X)`;
 }
 
 copyBtn.addEventListener("click", () => {
@@ -24,3 +33,7 @@ copyBtn.addEventListener("click", () => {
         tooltip.textContent = "Copy Code";
     }, 2000);
 });
+
+if (isHost) {
+    startBtn.disabled = false;
+}
--- frontend/online.js
+++ frontend/online.js
@@ -8,15 +8,85 @@ backBtn.addEventListener("click", () => {
     window.location.href = "index.html";
 });
 
+const usernameBtn = document.getElementById("username-btn");
+const usernamePanel = document.getElementById("username-panel");
+const saveUsernameBtn = document.getElementById("save-username");
+const usernameInput = document.getElementById("username-input");
+const usernameError = document.getElementById("username-error");
+const usernameDisplay = document.getElementById("username-display");
+let username = "";
+
+usernameBtn.addEventListener("click", () => {
+    usernamePanel.classList.toggle("active");
+
+    setTimeout(() => {
+        usernameInput.focus();
+    }, 200);
+});
+
+usernameInput.addEventListener("input", () => {
+    usernameError.style.display = "none";
+})
+
+saveUsernameBtn.addEventListener("click", () => {
+    let inputname = usernameInput.value.trim();
+
+    if(inputname === ""){
+        usernameError.style.display = "block";
+        return;
+    }
+
+    username = inputname;
+    usernameDisplay.textContent = username;
+
+    usernameBtn.style.display = "none";
+    usernameDisplay.style.display = "block";
+
+    usernameInput.value = "";
+    usernameError.textContent = "";
+    usernameError.style.display = "none";
+    usernamePanel.classList.remove("active");
+})
+usernameInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        saveUsernameBtn.click();
+    }
+});
+
+usernameDisplay.addEventListener("click", () => {
+    usernamePanel.classList.toggle("active");
+
+    setTimeout(() => {
+        usernameInput.focus();
+    }, 200);
+});
+
 function generateroomcode(){
     let code = Math.random().toString(36).substring(2,8).toUpperCase();
     return code;
 }
 
 createRoomBtn.addEventListener("click", () => {
+
+    if (username === "") {
+
+        usernameBtn.classList.add("shake");
+        usernameBtn.classList.add("flash-error");
+
+        setTimeout(() => {
+            usernameBtn.classList.remove("shake");
+            usernameBtn.classList.remove("flash-error");
+        }, 500);
+
+        usernamePanel.classList.add("active");
+        usernameInput.focus();
+    
+        return;
+    }
+
     const roomCode = generateroomcode();
 
-    window.location.href = `lobby.html?room=${roomCode}`;
+    window.location.href = `lobby.html?room=${roomCode}&host=true&name=${username}`;
 });
 
 roomCodeInput.addEventListener("input", () => {
@@ -26,6 +96,24 @@ roomCodeInput.addEventListener("input", () => {
 });
 
 joinRoomBtn.addEventListener("click", () => {
+
+    if (username === "") {
+
+        usernameBtn.classList.add("shake");
+        usernameBtn.classList.add("flash-error");
+
+        setTimeout(() => {
+            usernameBtn.classList.remove("shake");
+            usernameBtn.classList.remove("flash-error");
+        }, 500);
+
+        usernamePanel.classList.add("active");
+        usernameInput.focus();
+    
+        return;
+    }
+
+
     const inputcode = roomCodeInput.value;
 
     if(inputcode === ""){
@@ -39,5 +127,11 @@ joinRoomBtn.addEventListener("click", () => {
         return;
     }
 
-    window.location.href = `lobby.html?room=${inputcode}`;
+    window.location.href = `lobby.html?room=${inputcode}&host=false&name=${username}`;
 })
+
+roomCodeInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        joinRoomBtn.click();
+    }
+});
