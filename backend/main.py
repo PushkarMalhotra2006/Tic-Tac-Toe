cabf6f98109ad753965cee1fd6d3ca573fcf8517
PushkarMalhotra2006 <pushkarpmmal20@gmail.com>
2026-06-23 17:40:04 +0530

    added frontend url allow in cors

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["https://tic-tac-toe-multiplayer-ultra.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
